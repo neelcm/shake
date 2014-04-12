38912573a0ec8b766b7f2460da62ee7c18fec6bf
Neel Mouleeswaran <neelcm@ncm-mbp.local>
2014-04-12 02:21:51 -0500

    combined handshake code

--- pebble/src/shake.c
+++ pebble/src/shake.c
@@ -10,12 +10,125 @@ static GBitmap *icon_bitmap = NULL;
 static AppSync sync;
 static uint8_t sync_buffer[64];
 
+bool data_handshake;
+bool tap_handshake;
+
 enum ShakeKeys {
   ID_KEY = 0x0,         // TUPLE_INT
   NAME_KEY = 0x1,  // TUPLE_CSTRING
   PHONE_KEY = 0x2
 };
 
+void accel_tap_handler(AccelAxisType axis, int32_t direction) {
+  // Process tap on ACCEL_AXIS_X, ACCEL_AXIS_Y or ACCEL_AXIS_Z
+  // Direction is 1 or -1
+ 
+        //if (axis == ACCEL_AXIS_X && axis == ACCEL_AXIS_Y)
+                //APP_LOG(APP_LOG_LEVEL_ERROR, "X AND Y");
+ 
+        //if (axis == ACCEL_AXIS_Y && axis == ACCEL_AXIS_Z)
+   // APP_LOG(APP_LOG_LEVEL_ERROR, "Y AND Z");
+ 
+    if (axis == ACCEL_AXIS_Y)
+    {
+             APP_LOG(APP_LOG_LEVEL_ERROR, "Y AXIS BITCHES!");
+         APP_LOG(APP_LOG_LEVEL_ERROR, "handshake detected!");
+        tap_handshake = true;
+      //FUNCTION CALL FROM HERE for contact info
+    }
+ 
+  int x = direction;
+ 
+  char *str="xxxxxxxxxx";
+  snprintf(str, sizeof(str), "%d", x);
+  APP_LOG(APP_LOG_LEVEL_ERROR, str);
+}
+ 
+void handle_init(void) {
+  accel_tap_service_subscribe(&accel_tap_handler);
+}
+ 
+void handle_deinit(void) {
+  accel_tap_service_unsubscribe();
+}
+ 
+// batch
+void accel_data_handler(AccelData *data, uint32_t num_samples) {
+  // Process 10 events - every 1 second
+  
+  APP_LOG(APP_LOG_LEVEL_ERROR, "SAMPLE COLLECTED!");
+ 
+  int total_x = 0;
+  int total_y = 0;
+  int total_z = 0;
+  int avg_x = 0;
+  int avg_y = 0;
+  int avg_z = 0;
+  for(uint32_t i = 0; i < num_samples; i++)
+    {
+      total_x += (int)data[i].x;
+      total_y += (int)data[i].y;
+      total_z += (int)data[i].z;
+    }
+ 
+    avg_x = total_x/(int)num_samples;
+    avg_y = total_y/(int)num_samples;
+    avg_z = total_z/(int)num_samples;
+ 
+    char *str_x="xxxxxxxxxx";
+    snprintf(str_x, sizeof(str_x), "%d", avg_x);
+    APP_LOG(APP_LOG_LEVEL_ERROR, str_x);
+ 
+    char *str_y="xxxxxxxxxx";
+    snprintf(str_y, sizeof(str_y), "%d", avg_y);
+    APP_LOG(APP_LOG_LEVEL_ERROR, str_y);
+ 
+    char *str_z="xxxxxxxxxx";
+    snprintf(str_z, sizeof(str_z), "%d", avg_z);
+    APP_LOG(APP_LOG_LEVEL_ERROR, str_z);
+ 
+    if ((int)avg_y > 690)
+      {
+        if ( (int)avg_x < 200 )
+        {
+          if ( (int) avg_x < 200)
+            {
+              APP_LOG(APP_LOG_LEVEL_ERROR, "Proper handshake values detected!!");
+              data_handshake = true;
+            }
+        }
+         
+      }
+}
+ 
+ 
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed)
+{
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Time flies!");
+        if (data_handshake == true && tap_handshake == true)
+        {
+          APP_LOG(APP_LOG_LEVEL_ERROR, "Something unique");
+        }
+}
+ 
+ 
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  // register for batch sampling
+ 
+ 
+}
+ 
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+}
+ 
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
 
 static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
@@ -114,6 +227,16 @@ static void init(void) {
   app_message_open(inbound_size, outbound_size);
 
   const bool animated = true;
+
+  data_handshake = false;
+  tap_handshake = false;
+
+  accel_service_set_samples_per_update(25);
+  accel_data_service_subscribe(25, &accel_data_handler);
+  accel_tap_service_subscribe(&accel_tap_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+
   window_stack_push(window, animated);
 }
 
